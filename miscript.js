c9976b8d81ab4c32ad2fd6b448fd28bc2ae690d4
Euge <blythemme@gmail.com>
2020-09-30 16:29:56 -0300

    upd

---
+++ miscript.js
@@ -0,0 +1,32 @@
+
+/*var edad = Number(prompt("Ingresá tu edad"));
+
+if (edad < 18) {
+    alert("Usted es menor de edad");
+} else {
+    alert("Usted es mayor de edad");
+}
+
+var notaTesisAbogacia = 5;
+var notaTesisRedes = 9;
+
+
+if( ( notaTesisAbogacia == 5 ) && ( notaTesisRedes == 9 ) ) {
+	alert("Se cumplen las dos condiciones");
+} else {
+
+	alert("No se cumple alguna de las condiciones")
+}*/
+
+
+var notaTesis = prompt("Ingresa tu Nota de Tesis");
+
+if (notaTesis == 10) {
+	alert("Obtuviste la nota maxima");
+} else if {
+	(notaTesis < 4)
+		alert("Tu Tesis esta desaprobada");
+	} else {
+		(notaTesis > 4)
+		alert("Tu tesis esta aprobada");
+	}
