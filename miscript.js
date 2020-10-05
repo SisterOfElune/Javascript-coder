bd6dfac103739e81d4c3fc665fc1b56c2fd427bb
Euge <blythemme@gmail.com>
2020-10-05 16:15:48 -0300

    class

--- miscript.js
+++ miscript.js
@@ -18,6 +18,8 @@ if( ( notaTesisAbogacia == 5 ) && ( notaTesisRedes == 9 ) ) {
 	alert("No se cumple alguna de las condiciones")
 }*/
 
+/* IF ELSE CLASE 2
+
 var sign = prompt("Cual es tu signo?");
 
 if (sign == "Libra") {
@@ -30,3 +32,31 @@ if (sign == "Libra") {
 
 }
 
+*/
+/*sumar();
+
+function sumar(){
+ var numero1 = 3;
+ var numero2 = 5;
+ resultado = numero1 + numero2;
+ alert("El resultado es " + resultado);
+ }
+*/
+ //DIAS DE LA SEMANA
+
+ function mostrarDiasEnLaPantalla(){
+ 	var diasDeLaSemana = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'];
+ 	for(var i = 0; i < 7; i++){
+ 		console.log(diasDeLaSemana[i]);
+ 	}
+ }
+
+anime();
+ function anime(){
+ 	var anime;
+ 	var nombreProtagonista = "Asuna ";
+ 	var apellidoProtagonista = "Yuuki";
+
+ 	anime = nombreProtagonista + apellidoProtagonista;
+ 	console.log(anime);
+ }
