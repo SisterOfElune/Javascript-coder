d6eff14792f81b1c878b6e36fcb3588ebd8e710d
Euge <blythemme@gmail.com>
2020-10-07 10:08:48 -0300

    Update miscript.js

--- miscript.js
+++ miscript.js
@@ -57,7 +57,7 @@ function sumar(){
         }
 
         if (i % 2 == 0) {
-            pares[i] = "El dia " + dias[i] + " es par\n";
+            pares[i] = "El dia " + dias[i] +" es par\n";
         }
     }
     
